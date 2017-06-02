e92423207c5691534394572b0b3c4cc2c7c76ca0
Christopher Harrop <bannastre@gmail.com>
2017-06-02 09:47:37 +0100

    User can add tags to links

---
+++ spec/features/add_tag_spec.rb
@@ -0,0 +1,5 @@
+feature 'it allows a user to add a tag to a link' do
+  scenario 'user can see tags associated with links' do
+    
+  end
+end
--- spec/features/test_infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Test infrastructure' do
-  scenario 'Test infrastructure works' do
-    visit '/'
-    expect(page).to have_content("Hello Bookmarks!")
-  end
-end
