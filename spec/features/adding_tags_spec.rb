5527ddf29b76e9d0737a447763416bb798fe38c7
Christopher Harrop <bannastre@gmail.com>
2017-06-02 10:04:20 +0100

    User can add multiple tags

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -11,4 +11,14 @@ feature 'Adding tags' do
     expect(link.tags.map(&:name)).to include('education')
   end
 
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags',  with: 'education, coding'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'coding')
+  end
+
 end
